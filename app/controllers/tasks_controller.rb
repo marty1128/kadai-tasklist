d028e16de44579fa5a9545374e62eb564044dc09
masaki.hoashi <hoashi.masaki@gmail.com>
2019-11-24 10:47:20 +0000

    for submit heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-      @tasks = Task.all
+    #@tasks = Task.all
+    if logged_in? #Slack質問中ここから
+      @task = current_user.tasks.build  # form_with 用　
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end #Slack質問中ここまで
+    
   end
 
   def show
@@ -15,12 +21,14 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task = Task.new(task_params)
+      #@task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が作成されませんでした'
       render :new
     end
@@ -59,4 +67,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
